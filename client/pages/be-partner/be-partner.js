4b4ac826584f2074a8d4b53ccd56781098cfa2f5
jkliuyafei <jkshuma@qq.com>
2018-06-29 00:03:58 +0800

    11

--- client/pages/be-partner/be-partner.js
+++ client/pages/be-partner/be-partner.js
@@ -2,9 +2,9 @@
 /**
  * 本页面的功能是超级管理员转发给兄弟店来开通兄弟店，角色仅为superAdmin时才会显示转发按钮；
  * 兄弟店通过分享的链接打开后，填写email,password,name,phoneNumber后提交，后台把用
- * 户角色从user变成business，然后通过util.getUserInfo(1,callback)函数刷新本地userinfo
- * 缓存；用户角色变成business后，如果再次打开超级管理员分享的小程序页面，此时通过缓存判断
- * 角色为business,然后隐藏提交按钮，防止重复提交；
+ * 户角色从user变成partnerMerchant，然后通过util.getUserInfo(1,callback)函数刷新本地userinfo
+ * 缓存；用户角色变成partnerMerchant后，如果再次打开超级管理员分享的小程序页面，此时通过缓存判断
+ * 角色为partnerMerchant,然后隐藏提交按钮，防止重复提交；
  */
 var qcloud = require('../../vendor/wafer2-client-sdk/index')
 var config = require('../../config')
@@ -33,11 +33,13 @@ Page({
           adminShare:false,
         })
       }
+      //默认情况，用户成为合作商家
       if(userRole=='user'&&sceneData==1007){
         that.setData({
           openBtn:false,
         })
       }
+      //特殊情况，超级管理员代理成为合作商家
       if (userRole == 'agent' && sceneData == 1007) {
         that.setData({
           openBtn: false,
